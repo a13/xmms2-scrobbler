74c029544fa347e9be5aa74617e5ae5c6d3c3773
Tilman Sauerbeck <tilman@xmms.org>
2006-11-11 22:31:34 +0100

    Shorten this code a bit.

--- filters/musicbrainz.rb
+++ filters/musicbrainz.rb
@@ -1,6 +1,6 @@
 # filter out all tracks that don't have a musicbrainz artist id
 class MusicBrainzFilter < SubmissionFilter
 	def ignore?(propdict)
-		(propdict[:track_id] || "").length.zero?
+		(propdict[:track_id] || "").empty?
 	end
 end
